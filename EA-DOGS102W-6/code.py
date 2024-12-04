4cf29ae4023e88df24edd1fdb31aadc9f480bf29
neerajadsul <neeraj.adsul@gmail.com>
2024-12-04 18:26:51 +0000

    current working but random data

--- EA-DOGS102W-6/code.py
+++ EA-DOGS102W-6/code.py
@@ -33,21 +33,24 @@ class EaDogs102:
         self.spi = spi
         while not self.spi.try_lock():
             pass
-        # self.spi.configure(polarity=0)
+        self.spi.configure(polarity=1, phase=1)
         self.spi.unlock()
         self.chip_select = chip_select
         self.reset = reset
         self.data_mode = mode
-        self.data_mode = self.MODE_CMD
 
     def display_init(self):
+        self.reset.value = False
+        time.sleep(0.1)
+        self.reset.value = True
+        time.sleep(0.5)
         self.data_mode = self.MODE_CMD
         init_sequence = bytes([
             0x40, # display start line 0
             0xA1, # seg reverse
             0xC0, # Normal COM0 to COM63
             0xA4, # Set all pixel on
-            0xA6, # Inverse off
+#             0xA6, # Inverse off
             0xA2, # Set bias 1/9
             0x2F, # Booster, Regulator and Follower on
             # Set contrast
@@ -55,8 +58,8 @@ class EaDogs102:
             0x81, # Set electrostatic volume
             0x10,
             0xAF, # Display ON
-            0xFA, # Advanced program control 0,
-            0xA0,
+#             0xFA, # Advanced program control 0,
+#             0xA0,
         ])
         while not self.spi.try_lock():
             pass
@@ -80,34 +83,42 @@ class EaDogs102:
         self.spi.write(data)
         self.spi.unlock()
 
-    def show_data(self):
-
-        page_address = random.randrange(0, 8)
-        col_address_lsb = random.randrange(0, 16)
-        col_address_msb = random.randrange(0, 16)
-
-        commands = bytes([page_address, col_address_lsb, col_address_msb])
-
-        self._write_commands(commands)
-        some_data = bytes([
-            random.randrange(0, 127)
-        ])
-        self._write_data(some_data)
+    def show_data(self, data):
+        for col_address_lsb, col_address_msb in zip(range(0,7), range(0,7)): 
+            some_data = bytes([random.randrange(1, 255)])
+            for page_address in range(0, 8):
+                commands = bytes([
+#                     0xAE,
+                    page_address | 0xB0,
+                    col_address_lsb | 0x00,
+                    col_address_msb | 0x10,
+#                     0xAF,
+                ])
+                self._write_commands(commands)
+                time.sleep(0.1)
+                self._write_data(some_data)
+                time.sleep(0.05)
 
 
 
 display = EaDogs102(spi=spi, chip_select=chip_select, mode=data_mode, reset=reset_display)
-time.sleep(0.005)
+time.sleep(0.5) # Waiting for Vdd to rise for display
 print("Init Display")
 display.display_init()
 print("Display init complete.")
-display.show_data()
-print("Somedata")
 # display._write_commands(bytes([0xAA, 0xE2]))
+
+counter = 0
 while True:
     led.value = True
-    time.sleep(0.5)
+    time.sleep(1.5)
     led.value = False
-    time.sleep(0.5)
-    display.show_data()
+    time.sleep(1.5)
+    print(counter)
+    display.show_data(counter)
+    if counter > 6:
+        counter = 0
+    else:
+        counter += 1
+
 
